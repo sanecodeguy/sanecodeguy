2286989d3ae5f0da6ee1b1d8e971124cb70cee71
Syed Ali Rizvi <rizvicottage2@gmail.com>
2025-05-16 00:22:02 +0500

    Update update_views.py

--- update_views.py
+++ update_views.py
@@ -1,21 +1,23 @@
 import os
 from pymongo import MongoClient
 
+
 client = MongoClient(os.getenv("MONGODB_URI"))
 db = client["github_views"]
 collection = db["profile_views"]
 
+
 doc = collection.find_one({"username": "sanecodeguy"})
-if not doc:
-    collection.insert_one({"username": "sanecodeguy", "views": 0})
-    views = 0
-else:
-    views = doc["views"]
+views = doc["views"] if doc else 0
+new_views = views + 1
+
 
 collection.update_one(
     {"username": "sanecodeguy"},
-    {"$set": {"views": views + 1}}
+    {"$set": {"views": new_views}},
+    upsert=True
 )
 
+
 with open("views.txt", "w") as f:
-    f.write(str(views + 1))
+    f.write(str(new_views))
